976c1b597023789b7d34a989da6a3c4cd333e344
De Carli <decarm02@mssmcampus.mssm.edu>
2016-10-19 13:16:25 -0400

    add parameters to modelfit

--- R/modelfit.R
+++ R/modelfit.R
@@ -3,13 +3,21 @@
 #' @param dataframe a data.frame
 #' @param yvar character vector of the column name for outcome
 #' @param xvar character vector of the column name(s) for predictor(s)
-#'
+#' @param digits integer number of digits to print
+#' @param verbose logical whether to print summary
 #' @return a data.frame of the r-squared and the rmse for the linear regression
 #' @importFrom stats as.formula
 #' @importFrom stats lm
 #' @importFrom stats resid
+#' @examples
+#'test <- data.frame(x = 1:100, y = 1:100 + rnorm(100))
+#'modelfit(test, yvar = "y", xvar = "x")
 #' @export
-modelfit <- function(dataframe, yvar = "mean_pm25", xvar = "aod47") {
-  lmmod <- lm(as.formula(paste(y, "~", paste(x, collapse = "+"))), dataframe)
-  data.frame(broom::glance(lmmod)[1], rmse = sqrt(mean((resid(lmmod))^2)))
+modelfit <- function(dataframe, yvar = "pm", xvar = "aod47", digits = 2, verbose = TRUE) {
+  lmmod <- lm(as.formula(paste(yvar, "~", paste(xvar, collapse = "+"))), dataframe)
+  outdf <- data.frame(broom::glance(lmmod)[1], 
+                      rmse = sqrt(mean((resid(lmmod))^2)), 
+                      n = length(resid(lmmod)))
+  if(verbose) print(outdf, digits = digits)
+  invisible(outdf)
 }  
